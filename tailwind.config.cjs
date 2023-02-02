621f98d38fac939222b4dc575f281934105e14aa
riolly <alberto.riolly@gmail.com>
2023-02-02 10:20:16 +0700

    :recycle: lint & format

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			colors: {
-				accent: "rgb(124,58,237)",
+				accent: 'rgb(124,58,237)',
 			},
 			backgroundImage: {
-				"linear-accent":
-					"linear-gradient(45deg,theme(colors.accent),#da62c4 30%,white 60%)",
+				'linear-accent': 'linear-gradient(45deg,theme(colors.accent),#da62c4 30%,white 60%)',
 			},
 		},
 	},
 	plugins: [],
-};
+}
