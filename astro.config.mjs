91e1c1c011ac7cf962453d08452a998c54a4a618
riolly <alberto.riolly@gmail.com>
2023-02-04 10:17:52 +0700

    :wrench: tailwind applyBaseStyles: false

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import image from '@astrojs/image'
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
-		tailwind(),
+		tailwind({config: {applyBaseStyles: false}}),
 		image({
 			serviceEntryPoint: '@astrojs/image/sharp',
 		}),
