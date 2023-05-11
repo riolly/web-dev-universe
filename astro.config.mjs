97d3ddd51a2631dcb1402279ca476c441652f21c
riolly <alberto.riolly@gmail.com>
2023-05-12 01:27:24 +0700

    renew url

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://riolly.vercel.app',
+	site: 'https://www.riolly.dev',
 	integrations: [
 		tailwind({config: {applyBaseStyles: false}}),
 		image({
