0bd1c35340af8e3cd002408ad1cad4c4fb4c980a
riolly <alberto.riolly@gmail.com>
2023-05-15 15:06:24 +0700

    solid-js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,21 @@
-import {defineConfig} from 'astro/config'
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind'
+import image from '@astrojs/image';
 
 // https://astro.build/config
-import image from '@astrojs/image'
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.riolly.dev',
-	integrations: [
-		tailwind({config: {applyBaseStyles: false}}),
-		image({
-			serviceEntryPoint: '@astrojs/image/sharp',
-		}),
-	],
-})
+  site: 'https://www.riolly.dev',
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), solidJs()]
+});
