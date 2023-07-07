5d17135a9096fbd7765a8c89280b95dd7b205c68
riolly <alberto.riolly@gmail.com>
2023-07-07 13:08:06 +0700

    tailwind typography

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -40,5 +40,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwind-scrollbar")({ nocompatible: true })],
+  plugins: [
+    require("tailwind-scrollbar")({ nocompatible: true }),
+    require("@tailwindcss/typography"),
+  ],
 };
