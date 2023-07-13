7fe4d4530af2ed095ebf94ee7ebe512674e6f41e
riolly <alberto.riolly@gmail.com>
2023-07-13 21:43:40 +0700

    copy confirmation animation

--- src/components/elements/CopyOrOpenMenu.tsx
+++ src/components/elements/CopyOrOpenMenu.tsx
@@ -1,20 +1,30 @@
+import { For, createSignal, type JSX, Setter, Show } from "solid-js";
 import { DropdownMenu as Dropdown } from "@kobalte/core";
+import clsx from "clsx";
 
 import IconChevronDown from "components/icons/chevron-down";
 import IconCopy from "components/icons/copy";
-import { For, type JSX } from "solid-js";
+import IconCopySolid from "components/icons/copy-solid";
+
+import "./tada.css";
+
+// eslint-disable-next-line no-unused-vars
+type setterFn = (setOpen: Setter<boolean>) => void;
 
 type Props = {
   title: string | JSX.Element;
   options: {
-    onSelect: () => void;
     child: JSX.Element;
+    onSelect: setterFn;
+    closeOnSelect?: boolean;
   }[];
 };
 
 export default function CopyOrOpenMenu({ title, options }: Props) {
+  const [open, setOpen] = createSignal(false);
+
   return (
-    <Dropdown.Root>
+    <Dropdown.Root open={open()} onOpenChange={setOpen}>
       <Dropdown.Trigger class="flex items-center space-x-1 font-body text-sm leading-[1.1rem] text-slate-800 ui-expanded:underline dark:text-slate-300/75 md:text-base md:leading-tight lg:text-lg lg:leading-6">
         <Dropdown.Icon class="transition-transform ui-expanded:rotate-180">
           <IconChevronDown class="w-3 md:w-4" />
@@ -30,8 +40,9 @@ export default function CopyOrOpenMenu({ title, options }: Props) {
             {(option, index) => (
               <>
                 <Dropdown.Item
-                  class="flex justify-between gap-8 rounded-md px-2 py-1 hover:bg-slate-700 hover:text-slate-200 active:bg-slate-600 md:mt-0"
-                  onSelect={option.onSelect}
+                  class="min-w-[10rem] px-2 py-1 hover:cursor-pointer hover:font-bold md:mt-0"
+                  onSelect={() => option.onSelect(setOpen)}
+                  closeOnSelect={option.closeOnSelect}
                 >
                   {option.child}
                 </Dropdown.Item>
@@ -47,6 +58,43 @@ export default function CopyOrOpenMenu({ title, options }: Props) {
   );
 }
 
+function copyOption(text: string) {
+  const [copied, setCopied] = createSignal(false);
+  return {
+    onSelect: (setOpen: Setter<boolean>) => {
+      navigator.clipboard.writeText(text);
+      setCopied(true);
+      setTimeout(() => {
+        setOpen((o) => !o);
+        setCopied(false);
+      }, 900);
+    },
+    closeOnSelect: false,
+    child: (
+      <Show
+        when={copied()}
+        fallback={
+          <div
+            class={clsx(
+              "flex justify-between",
+              // not working, add delay or solid transition group
+              copied() && "animate-out fade-out-0 zoom-out-50 duration-1000",
+            )}
+          >
+            Copy
+            <IconCopy class="w-4" />
+          </div>
+        }
+      >
+        <div class="animation-tada flex justify-between duration-1000">
+          Copied
+          <IconCopySolid class="w-4 text-blue-800" />
+        </div>
+      </Show>
+    ),
+  };
+}
+
 export function CopyOrOpenPhone() {
   return (
     <CopyOrOpenMenu
@@ -56,29 +104,21 @@ export function CopyOrOpenPhone() {
         </>
       }
       options={[
-        {
-          onSelect: () => navigator.clipboard.writeText("+6285174328051"),
-          child: (
-            <>
-              Copy
-              <IconCopy class="w-4" />
-            </>
-          ),
-        },
+        copyOption("+6285174328051"),
         {
           onSelect: () =>
             window.open(
               "https://api.whatsapp.com/send?phone=6285174328051&&text=Hello+Alberto+%F0%9F%91%8B",
             ),
           child: (
-            <>
+            <div class="flex justify-between">
               Chat on WA
               <img
                 src="/images/whatsapp.svg"
                 alt="whatsapp logo"
                 class="-mr-1 w-6"
               />
-            </>
+            </div>
           ),
         },
       ]}
@@ -95,26 +135,17 @@ export function CopyOrOpenEmail() {
         </>
       }
       options={[
-        {
-          onSelect: () =>
-            navigator.clipboard.writeText("alberto.cahyantara@gmail.com"),
-          child: (
-            <>
-              Copy
-              <IconCopy class="w-4" />
-            </>
-          ),
-        },
+        copyOption("alberto.cahyantara@gmail.com"),
         {
           onSelect: () =>
             window.location.replace(
               "mailto:alberto.cahyantara@gmail.com?subject=Job%20Opportunity&body=Dear%20Alberto%20R%20Cahyantara%2CI%20came%20across%20your%20profile%20during%20my%20search%20for%20talented%20front-end%20developers%2C%20and%20I%20believe%20you%20would%20be%20a%20great%20fit%20for%20our%20company.%20We%20have%20an%20exciting%20job%20opportunity%20as%20a%20%5BFront-end%20Developer%5D%20at%20%5BCompany%20Name%5D.Position%3A%20%5BFront-end%20Developer%5DCompany%3A%20%5BCompany%20Name%5DLocation%3A%20%5BLocation%5DEmployment%20Type%3A%20%5BFull-time%2FPart-time%2FContract%5DSalary%3A%20Competitive%2C%20based%20on%20experienceIf%20you%20are%20interested%20in%20this%20opportunity%2C%20please%20reply%20to%20this%20email.%20We%20would%20love%20to%20discuss%20further%20details%20and%20schedule%20an%20interview%20with%20you.Looking%20forward%20to%20hearing%20from%20you!Best%20regards%2C%5BYour%20Name%5D%5BYour%20Title%2FPosition%5D%5BCompany%20Name%5D%5BContact%20Information%5D",
             ),
           child: (
-            <>
+            <div class="flex justify-between">
               Send email
               <img src="/images/gmail.svg" alt="gmail logo" class="w-5" />
-            </>
+            </div>
           ),
         },
       ]}
