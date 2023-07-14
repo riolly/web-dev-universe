a292c945096eee9497d662a20a981a0cbb27c725
riolly <alberto.riolly@gmail.com>
2023-07-14 14:06:24 +0700

    chore: unlighthouse

---
+++ unlighthouse.config.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference types="unlighthouse" />
+import { defineConfig } from "unlighthouse";
+
+export default defineConfig({
+  site: "riolly.dev",
+  debug: true,
+});
